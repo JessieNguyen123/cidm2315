5db2cbcf416a08f483997496f2de5f853f7320f0
JessieNguyen123 <nqnguyen3@buffs.wtamu.edu>
2025-09-29 12:25:01 -0500

    Homework5.cs

--- Homework5.cs
+++ Homework5.cs
@@ -1,3 +1,4 @@
+Question1-Code
 using System;
 
 namespace Homework5
@@ -21,3 +22,73 @@ namespace Homework5
     
     }
 }
+Question2-Code
+static int GetLargestNumber(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+        static int GetInteger(string prompt)
+        {
+            Console.Write(prompt);
+            string input = Console.ReadLine();
+            int.TryParse(input, out int number);
+            return number;
+        }
+        static int GetLargestofFour()
+        {
+            int a = GetInteger($"Enter value for (a): ");
+            int b = GetInteger($"Enter value for (b): ");
+            int c = GetInteger($"Enter value for (c): ");
+            int d = GetInteger($"Enter value for (d): ");
+            Console.WriteLine($"a = {a}; b = {b}; c = {c}; d = {d}");
+            int max1 = GetLargestNumber(a, b);
+            int max2 = GetLargestNumber(c, d);
+            int max = GetLargestNumber(max1, max2);
+            return max;
+        }        
+        static void Main(string[] args)
+        {
+            int largest = GetLargestofFour();
+            Console.WriteLine($"The largest number is: {largest}");
+        }
+Question3-Code
+{
+    static void Main(string[]args)
+    {
+        createAccount();
+    }
+    static bool checkAge(int birth_year)
+    {
+        int current_year = 2025;
+        int age = current_year - birth_year;
+        return age >= 18;
+    }
+    static void createAccount()
+    {
+        Console.Write($"Enter username: ");
+        string? username = Console.ReadLine();
+        Console.Write("Enter password: ");
+        string? password = Console.ReadLine();
+        Console.Write("Re-enter password: ");
+        string? confirmPassword = Console.ReadLine();
+        Console.Write("Enter your birth year: ");
+        string? input = Console.ReadLine();
+        int.TryParse(input, out int birthYear);
+        if (checkAge(birthYear))
+        {
+            if (password == confirmPassword)
+            {
+                Console.WriteLine($"Account is created successfully");
+            }
+            else
+            {
+                Console.WriteLine($"Wrong password");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Could not create an account");
+        }
+    }
+    
+    }
